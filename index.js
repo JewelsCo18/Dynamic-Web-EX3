8a9fab1d6628f4d03550fe1d743c10e2caa4f44a
Jules Kho <juleskho@10-18-173-75.dynapool.wireless.nyu.edu>
2021-10-25 11:26:49 -0400

    comments for understanding

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const app = express();
 const homePage = require('./routes/index.js'); 
 const aboutPage = require('./routes/about.js');
 
+//location on system we run the code could really use any port 
 const port = 3000; 
 
+//allows access to public folder (i.e. our picture)
 app.use(express.static("public")); 
+
 app.use('/', homePage); 
 app.use('/about', aboutPage); 
 
+//when running file, listen on port, and output console log
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
